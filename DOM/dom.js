a83cdc5d3f5d62d0431c8a6db3c8c78ca195d2cd
darshan-1798 <112754624+darshan-1798@users.noreply.github.com>
2023-05-21 21:40:07 +0530

    Done with Changes

---
+++ DOM/dom.js
@@ -0,0 +1,21 @@
+// console.dir(document);
+
+console.log(document.domain);
+console.log(document.URL);
+// console.log(document.title);
+document.title = "Darshan Form";
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+// console.log(document);
+console.log(document.body);
+
+var headerTitle = document.getElementById("header-title");
+
+headerTitle.style.borderBottom = "solid 3px #000";
+
+h2.style.fontWeight = "bold";
+
+var items = document.getElementsByClassName("title");
+items.style.fontWeight = "bold";
+items.style.color = "green";
