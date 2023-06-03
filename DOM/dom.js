862d44029e49850e0fc03d4e7a582f449c54070f
darshan-1798 <112754624+darshan-1798@users.noreply.github.com>
2023-06-03 17:28:04 +0530

    4th question done

--- DOM/dom.js
+++ DOM/dom.js
@@ -14,8 +14,22 @@ var headerTitle = document.getElementById("header-title");
 
 headerTitle.style.borderBottom = "solid 3px #000";
 
-h2.style.fontWeight = "bold";
+// h2.style.fontWeight = "bold";
 
-var items = document.getElementsByClassName("title");
-items.style.fontWeight = "bold";
-items.style.color = "green";
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
+
+console.log(items[1]);
+items[1].style.fontWeight = "bold";
+
+for (var i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = "yellow";
+}
+
+var li = document.getElementsByTagName("li");
+console.log(li);
+console.log(li[1]);
+
+for (let i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = "#f4f4f4";
+}
